1af250e6d567eca86f89bd8d8b63f4d80d1c3557
Ali Valipour <ali.valipour@hotmail.com>
2025-08-15 19:25:13 +0330

    feat(ui): update Experience component layout and styles
    Refactor the Experience component to improve layout and styles,
    replacing absolute positioning with a responsive flex layout
    for better adaptability on different screen sizes. Simplified
    the HTML structure and updated class names for clarity.
    Additionally, re-enabled the Experience component in the main
    page.

--- src/app/modules/experience.tsx
+++ src/app/modules/experience.tsx
@@ -1,46 +1,51 @@
+import { MoveRightIcon } from "lucide-react";
+import Image from "next/image";
+
 import { Button } from "@/components/ui/button";
 
 export const Experience = () => {
   return (
     <>
-      <section className="absolute w-[728px] h-[591px] top-[2650px] left-[-65px]">
-        <img
-          className="absolute w-[625px] h-[523px] top-[68px] left-[65px] rounded-[49px] blur-[25px] object-cover"
-          alt="Rectangle"
-          src="/rectangle-1447.png"
-        />
-        <div className="absolute w-[495px] h-[422px] top-0 left-0 bg-[#f7f7f7] rounded-[20px]" />
-        <div className="absolute w-[495px] h-[301px] top-[138px] left-[233px] bg-[#f7f7f7] rounded-[20px]" />
-        <img
-          className="absolute w-[602px] h-[445px] top-[66px] left-[65px]"
-          alt="Rectangle"
-          src="/rectangle-1446.png"
-        />
+      <section className="flex w-full flex-col gap-12 p-12 md:flex-row md:gap-20 md:px-0 md:py-60">
+        <div className="relative min-h-[300px] md:h-[674px] md:w-1/3 md:max-w-[690px]">
+          <div className="absolute end-56 top-0 hidden h-[422px] w-[495px] rounded-[20px] bg-[#f7f7f7]" />
+          <div className="absolute end-0 top-[138px] hidden h-[300px] w-[494px] rounded-[20px] bg-[#f7f7f7]" />
+          <Image
+            className="absolute top-8 rounded-[50px] object-cover opacity-50 blur-[25px] md:end-[88px] md:top-[118px] md:h-[424px] md:w-[556px]"
+            height={424}
+            width={556}
+            alt="Rectangle"
+            src="/experience-banner.jpg"
+          />
+          <Image
+            className="absolute rounded-[20px] object-cover md:end-[60px] md:top-[66px] md:h-[446px] md:w-[630px]"
+            height={446}
+            width={630}
+            alt="Rectangle"
+            src="/experience-banner.jpg"
+          />
+        </div>
+        <div className="flex flex-col gap-5 md:pt-20">
+          <div className="text-lg font-semibold text-[#e58411]">
+            EXPERIENCES
+          </div>
+          <div className="w-full text-2xl font-bold text-[#1e1e1e] md:w-[412px] md:text-[42px]">
+            We Provide You The Best Experience
+          </div>
+          <p className="max-w-[556px] leading-[185%] font-normal text-[#1e1e1e] opacity-80 md:text-lg">
+            You don&apos;t have to worry about the result because all of these
+            interiors are made by people who are professionals in their fields
+            with an elegant and lucurious style and with premium quality
+            materials
+          </p>
+          <div className="flex items-center gap-3 text-[#e58311]">
+            <Button variant="link" className="p-0 text-sm font-medium">
+              More Info
+            </Button>
+            <MoveRightIcon />
+          </div>
+        </div>
       </section>
-      <div className="absolute w-[413px] top-[2774px] left-[743px] [font-family:'Gilroy-Bold-Bold',Helvetica] font-bold text-[#1e1e1e] text-[42px] tracking-[0] leading-[normal]">
-        We Provide You The Best Experience
-      </div>
-      <p className="absolute w-[556px] top-[2898px] left-[743px] opacity-80 [font-family:'Gilroy-Regular-Regular',Helvetica] font-normal text-[#1e1e1e] text-lg tracking-[0] leading-[33.3px]">
-        You don&apos;t have to worry about the result because all of these
-        interiors are made by people who are professionals in their fields with
-        an elegant and lucurious style and with premium quality materials
-      </p>
-      <div className="absolute w-[123px] h-[26px] top-[3055px] left-[743px]">
-        <Button
-          variant="link"
-          className="p-0 h-auto text-[#e58311] [font-family:'Gilroy-Medium-Medium',Helvetica] font-medium text-sm tracking-[0] leading-[25.9px]"
-        >
-          More Info
-        </Button>
-        <img
-          className="absolute w-12 h-6 top-px left-[73px]"
-          alt="Grommet icons form"
-          src="/grommet-icons-form-next-link.svg"
-        />
-      </div>
-      <div className="absolute top-[2732px] left-[743px] [font-family:'Gilroy-SemiBold-SemiBold',Helvetica] font-semibold text-[#e58411] text-lg tracking-[3.15px] leading-[normal]">
-        EXPERIENCES
-      </div>
     </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       <WhyChoosingUs />
       <Products />
-      {/* <Experience /> */}
+      <Experience />
       {/* <Materials /> */}
       {/* <Testimonials /> */}
       {/* <Footer /> */}
